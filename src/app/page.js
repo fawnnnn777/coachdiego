95b6c2ab3cb0f9222fdfe414aacdb9a87616315d
Diego <ale.rgdap@gmail.com>
2025-01-13 19:58:08 -0600

    add video

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,24 @@ import { raleway, ralewaylight } from "./ui/fonts/fonts";
 import { montserrat } from "./ui/fonts/fonts";
 
 export default function Home() {
+
+
   return (
     <div className="bg-main h-fit box-border pb-20 lg:h-full lg:pb-0">
       <div>
         <h1 className={`${raleway.className} antialiased text-6xl p-5`}>Es hora de aprender Inglés con el Coach Diego</h1>
         <div className="w-full flex">
-          <p className={`${ralewaylight.className} antialised text-2xl p-5`}>Con 5 anios de experiencia, ensenando ingles y ayudando a personas alrededor del mundo</p>
+          <p className={`${ralewaylight.className} antialised text-2xl p-5`}>¡Con 5 años de experiencia enseñando Inglés y ayudando a personas de todo el mundo!</p>
         </div>
-          <Image
+         {/* <Image
           src={'/me.png'}
           width={250}
           height={250}
-          ></Image>
+          ></Image>*/}
+          <iframe className="mx-auto" src="https://www.youtube.com/watch?v=1agk_MfSoQM"> </iframe>
+      </div>
+      <div id="benefits">
+        <h1>muchos benefits</h1>
       </div>
     </div>
   );
--- src/app/ui/home/Navbar.jsx
+++ src/app/ui/home/Navbar.jsx
@@ -1,12 +1,24 @@
+"use client"
 import Image from "next/image"
 import { montserrat } from "../fonts/fonts"
 
 export default function Navbar(){
+
+    const scrollToSection = (id) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
     return (
     <div className={`h-16 lg:h-20 w-screen bg-black text-yellow box-border flex items-center justify-between p-5`}>
-        <h1 className={`text-white p-5 text-2xl ${montserrat.className}`}>Coach Diego</h1>
-        <button className={`rounded-2xl bg-white m-2 p-2 border-2 border-white text-blue-500 
-            ${montserrat.className}`}>Más Información</button>
+        <button onClick={() => scrollToSection("benefits")} className={`rounded-2xl bg-white m-2 p-2 text-blue-500 
+            ${montserrat.className}`}>Beneficios</button>
+        <button className={`rounded-2xl bg-white m-2 p-2 text-blue-500 
+            ${montserrat.className}`}>Precios</button>
+        <button className={`rounded-2xl bg-yellow-500 m-2 p-2 text-white 
+            ${montserrat.className}`}>Contact</button>
     </div>
     )
 }
