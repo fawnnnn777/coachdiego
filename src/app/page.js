e0e03675c63a1a84aec972c30586f432e5957b4c
Diego Rojas <diegorojas@Diegos-Mac-mini.local>
2025-04-14 10:19:58 -0600

    new video for real

--- src/app/page.js
+++ src/app/page.js
@@ -15,8 +15,7 @@ export default function Home() {
         <div className="w-full flex">
           <p className={`${ralewaylight.className} antialised text-2xl p-5`}>¡Con 5 años de experiencia enseñando Inglés y ayudando a personas de todo el mundo!</p>
         </div>
-      <iframe className="mx-auto" width="320" height="180" src="https://www.youtube.com/watch?v=sJmNw-67R24" title="B3 CLASS EXAMPLE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      </div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/sJmNw-67R24?si=42XDs_u4W7WuoJFJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>      </div>
       <div id="benefits" className="mt-8">
         <h1 className={`${raleway.className} antialiased text-5xl p-5 `} >¡Con la calidad de academias reconocidas mundialmente, a la mitad del precio!</h1>
         <p className={`${ralewaylight.className} antialised text-2xl p-5`}>¡Quiero ayudarte a conseguir tu meta este año! Ya sea que quieras un <strong>nuevo trabajo, un ascenso o simplemente quieras ser bilingüe.</strong></p>
