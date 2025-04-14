21c7cbbb4365ff08f9a0b4d7c8e39e1cd9d93b86
Diego Rojas <diegorojas@Diegos-Mac-mini.local>
2025-04-14 10:26:21 -0600

    new video for real real

--- src/app/page.js
+++ src/app/page.js
@@ -12,10 +12,10 @@ export default function Home() {
         <div className="">
         <h1 className={`${raleway.className} antialiased text-6xl p-5`}>Es hora de aprender Inglés con el Coach Diego</h1>
         </div>
-        <div className="w-full flex">
+        <div className="w-full flex flex-col justify-center items-center">
           <p className={`${ralewaylight.className} antialised text-2xl p-5`}>¡Con 5 años de experiencia enseñando Inglés y ayudando a personas de todo el mundo!</p>
+          <iframe width="360" height="201" src="https://www.youtube.com/embed/sJmNw-67R24?si=42XDs_u4W7WuoJFJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>      </div>
         </div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/sJmNw-67R24?si=42XDs_u4W7WuoJFJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>      </div>
       <div id="benefits" className="mt-8">
         <h1 className={`${raleway.className} antialiased text-5xl p-5 `} >¡Con la calidad de academias reconocidas mundialmente, a la mitad del precio!</h1>
         <p className={`${ralewaylight.className} antialised text-2xl p-5`}>¡Quiero ayudarte a conseguir tu meta este año! Ya sea que quieras un <strong>nuevo trabajo, un ascenso o simplemente quieras ser bilingüe.</strong></p>
